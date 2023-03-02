90d6c617a97741feac4004b1706473b07c03a9a6
Shamima2 <shamima2islam@gmail.com>
2023-03-02 18:57:59 -0500

    First Push by Borsha

---
+++ Borsha_Practice/src/test/java/com/borsha/Variable_Practice.java
@@ -0,0 +1,15 @@
+	package com.borsha;
+	
+	public class Variable_Practice {
+	
+		public static void main(String[] args) {
+			
+		String var = "This is instance variable";
+		int x = 10;
+		
+	System.out.println(var);
+	System.out.println();
+	System.out.println(x);
+		}
+	
+	}
